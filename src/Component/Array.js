726418d8a16b0d4fa0a3167736657950bd32ddac
Vaibhav9520 <vaibhavsingh01080@gmail.com>
2024-09-22 09:52:04 +0530

    update

--- src/Component/Array.js
+++ src/Component/Array.js
@@ -1,17 +1,61 @@
 import React from 'react';
 
 const ArrayOperations = ({ numbers }) => {
+  // // greater than 5
+  // const firstGreaterThanFive = numbers.find(num => num > 5);
+
+  // //greater than 8
+  // const someGreaterThanEight = numbers.some(num => num > 8);
+
+  // //less than 11
+  // const everyLessThanEleven = numbers.every(num => num < 11);
+
+  // // descending order
+  // const sortedArray = [...numbers].sort((a, b) => b - a);
+
   // greater than 5
-  const firstGreaterThanFive = numbers.find(num => num > 5);
+  let firstGreaterThanFive;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 5) {
+      firstGreaterThanFive = numbers[i];
+      break; 
+  }
+}
 
-  //greater than 8
-  const someGreaterThanEight = numbers.some(num => num > 8);
 
-  //less than 11
-  const everyLessThanEleven = numbers.every(num => num < 11);
+// greater than 8
+  let someGreaterThanEight = false;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 8) {
+     someGreaterThanEight = true;
+     break; 
+  }
+}
 
-  // descending order
-  const sortedArray = [...numbers].sort((a, b) => b - a);
+
+// less than 11
+  let everyLessThanEleven = true;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] >= 11) {
+      everyLessThanEleven = false;
+      break; 
+  }
+}
+
+
+// descending order
+  let sortedArray = [...numbers];
+  for (let i = 0; i < sortedArray.length - 1; i++) {
+   for (let j = i + 1; j < sortedArray.length; j++) {
+     if (sortedArray[i] < sortedArray[j]) {
+      // Swap elements
+        let temp = sortedArray[i];
+        sortedArray[i] = sortedArray[j];
+        sortedArray[j] = temp;
+    }
+  }
+}
+
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Array from './Component/Array'
 
-import TodoList from './Component/TodoList'
+// import TodoList from './Component/TodoList'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <TodoList />
+  <Array />
   </React.StrictMode>
 );
 
